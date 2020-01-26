540a4a00b168b6cfe6593ad5d7917ee5ad69ab5b
ubulina <ukokko@gmail.com>
2020-01-26 21:32:47 +0200

    Ensimmäinen tallennus

---
+++ src/main/java/fi/demot/hellospring/web/HelloController.java
@@ -0,0 +1,23 @@
+package fi.demot.hellospring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+	@RequestMapping("/index")
+	public String giveIndex() {
+
+		return "This is the main page";
+	}
+
+	@RequestMapping("/contact")
+	public String giveContact() {
+
+		return "This is the contact page";
+	}
+
+}
